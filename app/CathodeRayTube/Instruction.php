35dcefff4ded63c62fcffee4fa445ee7dbca342f
Steve Adams <irwtdvoys@gmail.com>
2022-12-10 15:49:22 +0000

    Implementation

---
+++ app/CathodeRayTube/Instruction.php
@@ -0,0 +1,21 @@
+<?php
+	namespace App\CathodeRayTube;
+
+	class Instruction
+	{
+
+		public string $operation;
+		public ?int $value = null;
+
+		public function __construct(string $data)
+		{
+			$parts = str_split($data, 4);
+			$this->operation = $parts[0];
+
+			if (isset($parts[1]))
+			{
+				$this->value = (int)trim($parts[1]);
+			}
+		}
+	}
+?>
